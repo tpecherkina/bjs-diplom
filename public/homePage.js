65dd6eecc45bdecc2920f665264cae6224d6c75c
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-11-17 20:54:46 -0500

    Новые методы

--- public/homePage.js
+++ public/homePage.js
@@ -16,3 +16,30 @@ ApiConnector.current((response) => {
     ProfileWidget.showProfile(response.data);
   }
 });
+
+// Текущие курсы валют
+let ratesBoard = new RatesBoard;
+function getStocks() {
+	ApiConnector.getStocks((response) => {
+		if (response.success) {
+			ratesBoard.clearTable();
+			ratesBoard.fillTable(response.data);
+		}
+	})
+}
+
+
+// Операции с деньгами
+let moneyManager = new MoneyManager;
+
+// Пополнить баланс
+moneyManager.addMoneyCallback = (data) => {
+	ApiConnector.addMoney(data, (response) => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Ваш баланс пополнен успешно');
+		} else {
+			moneyManager.setMessage(response.error);
+		}
+	});
+}
